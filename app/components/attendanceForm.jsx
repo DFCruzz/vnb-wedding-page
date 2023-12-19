7b47bcfd290209754a5d13ee89d44dea95b75c71
Diego Cruz <diegofcruzz@gmail.com>
2023-12-19 11:55:33 -0300

    attendanceForm function disabled

--- app/components/attendanceForm.jsx
+++ app/components/attendanceForm.jsx
@@ -27,7 +27,7 @@ const AttendanceForm = () => {
     const emailData = {
       Recipients: [
         {
-          Email: process.env.NEXT_PUBLIC_EMAIL_RECEIVER,
+          Email: "mockreceiver@mockup.com",
           Fields: {
             name: `${fullName}`
           }
@@ -58,7 +58,7 @@ const AttendanceForm = () => {
                       Em qual evento você irá comparecer? ${eventType}`,
           },
         ],
-        From: process.env.NEXT_PUBLIC_EMAIL_SENDER,
+        From: "mocksender@mockup.com",
         Subject: `${fullName} - Confirmação de Presença`,
       },
     };
@@ -214,8 +214,9 @@ const AttendanceForm = () => {
         <button
           type="submit"
           className="text-white bg-[#859f7e] hover:bg-lime-950 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-64 sm:w-auto px-5 py-2.5 text-center"
+          disabled
         >
-          Confirmar Presença
+          Botão desabilitado
         </button>
       </form>
     </>
